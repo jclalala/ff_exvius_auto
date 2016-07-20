42a8623576a3a6f3932e594a10f6fa7ada324c5a
Jeffrey Chang <jeffrey@stiteq.com>
2016-07-20 08:51:19 +0800

    use exec to run commands

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 "use strict";
 
 var later = require('later');
-var Mocha = require('mocha');
+var exec = require('exec');
 
-var textSched = later.parse.text('every ' + 50 + ' min');
-later.setInterval(runCollectUpgradeItems, textSched);
+// set run schedule here...
+var sched = later.parse.recur().on(30).minute();
+later.setInterval(runCollectUpgradeItems, sched);
+
+console.log("Next run is scheduled at;" + later.schedule(sched).next(1));
+console.log("Init index.js at;" + new Date());
 
 function runCollectUpgradeItems() {
-  var mocha = new Mocha();
-  mocha.addFile("ff_exvius.js");
-  mocha.run();
+  // exec npm test
+  exec(['npm', 'test'], function (err, out, code) {
+    if (err instanceof Error)
+      throw err;
+    process.stderr.write(err);
+    process.stdout.write(out);
+
+    console.log("Completed ff_exvius.js at;" + new Date());
+    console.log("Next run is scheduled at;" + later.schedule(sched).next(1));
+
+    process.exit(code);
+  });
 };
